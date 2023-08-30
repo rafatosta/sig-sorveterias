6a58915f026f1ed5e994f1b56c7d98767644fe84
Rafael Tosta <rafa.ecomp@gmail.com>
2023-08-30 09:46:29 -0300

    :construction: EstoqueDAOList

---
+++ src/main/java/com/uefs/sigsorveterias/dao/estoque/EstoqueDAOList.java
@@ -0,0 +1,37 @@
+package com.uefs.sigsorveterias.dao.estoque;
+
+import com.uefs.sigsorveterias.model.Estoque;
+
+import java.util.List;
+
+public class EstoqueDAOList implements EstoqueDAO{
+    @Override
+    public Estoque create(Estoque obj) {
+        return null;
+    }
+
+    @Override
+    public void delete(Estoque obj) {
+
+    }
+
+    @Override
+    public void deleteMany() {
+
+    }
+
+    @Override
+    public Estoque update(Estoque obj) {
+        return null;
+    }
+
+    @Override
+    public List<Estoque> findMany() {
+        return null;
+    }
+
+    @Override
+    public Estoque findById(int id) {
+        return null;
+    }
+}
