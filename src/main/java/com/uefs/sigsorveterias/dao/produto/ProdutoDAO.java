3eeb772ba87497f126633bc02fa07a8f9b1522ea
Rafael Tosta <rafa.ecomp@gmail.com>
2023-08-30 09:29:34 -0300

    :sparkles: feat: Produto DAO
    obs.: Os métodos herdados em CRUD ainda existem, pois foram realizadas apenas sobrecarga de métodos. Essa sobrecarga está relacionada às especificidades entre o relacionamento entre a classe Produto e Estoque.

--- src/main/java/com/uefs/sigsorveterias/dao/produto/ProdutoDAO.java
+++ src/main/java/com/uefs/sigsorveterias/dao/produto/ProdutoDAO.java
@@ -1,7 +1,35 @@
 package com.uefs.sigsorveterias.dao.produto;
 
 import com.uefs.sigsorveterias.dao.CRUD;
+import com.uefs.sigsorveterias.model.Estoque;
 import com.uefs.sigsorveterias.model.Produto;
 
+import java.util.List;
+
 public interface ProdutoDAO extends CRUD<Produto> {
+
+    /**
+     * Retorna todos os produtos de estoque específico
+     *
+     * @param estoque
+     * @return
+     */
+    public List<Produto> findManyByEstoque(Estoque estoque);
+
+    /**
+     * Retorna um produto de um estoque específico
+     *
+     * @param idProduto
+     * @param estoque
+     * @return
+     */
+    public Produto findById(int idProduto, Estoque estoque);
+
+
+    /**
+     * Remove todos os produtos de um estoque
+     *
+     * @param estoque
+     */
+    public void deleteMany(Estoque estoque);
 }
