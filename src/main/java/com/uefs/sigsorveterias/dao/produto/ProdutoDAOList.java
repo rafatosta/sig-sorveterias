e259542342c503155a8f47eb144a6a2c573f0364
Rafael Tosta <rafa.ecomp@gmail.com>
2023-08-30 09:46:18 -0300

    :construction: ProdutoDAOList

--- src/main/java/com/uefs/sigsorveterias/dao/produto/ProdutoDAOList.java
+++ src/main/java/com/uefs/sigsorveterias/dao/produto/ProdutoDAOList.java
@@ -1,18 +1,25 @@
 package com.uefs.sigsorveterias.dao.produto;
 
+import com.uefs.sigsorveterias.model.Estoque;
 import com.uefs.sigsorveterias.model.Produto;
 
 import java.util.List;
 
 public class ProdutoDAOList implements ProdutoDAO{
+
     @Override
     public Produto create(Produto obj) {
         return null;
     }
 
     @Override
-    public Produto delete(Produto obj) {
-        return null;
+    public void delete(Produto obj) {
+
+    }
+
+    @Override
+    public void deleteMany() {
+
     }
 
     @Override
@@ -21,7 +28,27 @@ public class ProdutoDAOList implements ProdutoDAO{
     }
 
     @Override
-    public List<Produto> read() {
+    public List<Produto> findMany() {
         return null;
+    }
+
+    @Override
+    public Produto findById(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Produto> findManyByEstoque(Estoque estoque) {
+        return null;
+    }
+
+    @Override
+    public Produto findById(int idProduto, Estoque estoque) {
+        return null;
+    }
+
+    @Override
+    public void deleteMany(Estoque estoque) {
+
     }
 }
