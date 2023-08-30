9d901873b318af28dd36c38a79c0b207c6efd99f
Rafael Tosta <rafa.ecomp@gmail.com>
2023-08-30 09:30:09 -0300

    :sparkles: feat: EstoqueDAO

---
+++ src/main/java/com/uefs/sigsorveterias/dao/estoque/EstoqueDAO.java
@@ -0,0 +1,12 @@
+package com.uefs.sigsorveterias.dao.estoque;
+
+import com.uefs.sigsorveterias.dao.CRUD;
+import com.uefs.sigsorveterias.dao.produto.ProdutoDAOList;
+import com.uefs.sigsorveterias.model.Estoque;
+
+import java.util.List;
+
+public interface EstoqueDAO extends CRUD<Estoque> {
+
+
+}
