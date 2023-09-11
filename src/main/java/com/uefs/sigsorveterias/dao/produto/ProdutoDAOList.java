19899aaef36785384a93fff4562dee197865366e
Rafael Tosta <rafa.ecomp@gmail.com>
2023-09-11 11:32:44 -0300

    :sparkles: feat: add implementação ProdutoDAOList

--- src/main/java/com/uefs/sigsorveterias/dao/produto/ProdutoDAOList.java
+++ src/main/java/com/uefs/sigsorveterias/dao/produto/ProdutoDAOList.java
@@ -1,54 +1,96 @@
 package com.uefs.sigsorveterias.dao.produto;
 
+import com.uefs.sigsorveterias.model.Produto;
 import com.uefs.sigsorveterias.model.Estoque;
 import com.uefs.sigsorveterias.model.Produto;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class ProdutoDAOList implements ProdutoDAO{
+public class ProdutoDAOList implements ProdutoDAO {
+
+    private List<Produto> lista;
+
+    private int proximoID;
+
+    public ProdutoDAOList() {
+        this.lista = new ArrayList<>();
+        this.proximoID = 0;
+    }
+
+    /**
+     * Retorna ID para o objeto atual e define o próximo ID
+     *
+     * @return
+     */
+    private int getProximoID() {
+        return this.proximoID++;
+    }
 
     @Override
     public Produto create(Produto obj) {
-        return null;
+        obj.setId(this.getProximoID());
+        this.lista.add(obj);
+        return obj;
     }
 
     @Override
     public void delete(Produto obj) {
-
+        this.lista.remove(obj);
     }
 
     @Override
     public void deleteMany() {
-
+        this.lista = new ArrayList<>();
+        this.proximoID = 0;
     }
 
     @Override
     public Produto update(Produto obj) {
-        return null;
+        int index = this.lista.indexOf(obj);
+        this.lista.set(index, obj);
+        return obj;
     }
 
     @Override
     public List<Produto> findMany() {
-        return null;
+        return this.lista;
     }
 
     @Override
     public Produto findById(int id) {
+        for (Produto Produto : this.lista) {
+            if (Produto.getId() == id) {
+                return Produto;
+            }
+        }
         return null;
     }
 
     @Override
     public List<Produto> findManyByEstoque(Estoque estoque) {
-        return null;
+        List<Produto> pEstoque = new ArrayList<>();
+        for (Produto p : this.lista) {
+            if (p.getEstoque().equals(estoque)) {
+                pEstoque.add(p);
+            }
+        }
+        return pEstoque;
     }
 
     @Override
     public Produto findById(int idProduto, Estoque estoque) {
+        for (Produto p : this.lista) {
+            if (p.getId() == idProduto && p.getEstoque().equals(estoque)) {
+                return p;
+            }
+        }
         return null;
     }
 
     @Override
     public void deleteMany(Estoque estoque) {
-
+        this.lista = new ArrayList<>();
+        this.proximoID = 0;
     }
 }
