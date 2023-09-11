17e3bdd58a77b30357cf7d245e0e1f1140c68512
Rafael Tosta <rafa.ecomp@gmail.com>
2023-09-11 20:36:29 -0300

    :wrench: chore: Permite o acesso aos pacotes para os testes de unidade pelo TestSuite

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,19 @@ module com.uefs.sigsorveterias {
     requires javafx.controls;
     requires javafx.fxml;
 
+    /**
+     * Permite o acesso aos pacotes para os testes de unidade pelo TestSuite
+     */
+    opens com.uefs.sigsorveterias.dao.cliente;
+    opens com.uefs.sigsorveterias.dao.estoque;
+    opens com.uefs.sigsorveterias.dao.funcionario;
+    opens com.uefs.sigsorveterias.dao.evento;
+    opens com.uefs.sigsorveterias.dao.pedido;
+    opens com.uefs.sigsorveterias.dao.item_pedido;
+    opens com.uefs.sigsorveterias.dao.produto;
+    opens com.uefs.sigsorveterias.dao.venda;
+    opens com.uefs.sigsorveterias.model;
+    /** */
 
     opens com.uefs.sigsorveterias to javafx.fxml;
     exports com.uefs.sigsorveterias;
