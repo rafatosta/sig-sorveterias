fcf7ac4f0225eb308f629c5bad8f5250e6cee4a4
Rafael Tosta <rafa.ecomp@gmail.com>
2023-09-11 21:03:14 -0300

    :sparkles: feat: add TestSuite

---
+++ src/test/java/com/uefs/sigsorveterias/TestSuite.java
@@ -0,0 +1,10 @@
+package com.uefs.sigsorveterias;
+
+import org.junit.platform.suite.api.*;
+
+@SelectPackages({
+        "com.uefs.sigsorveterias.dao",
+        "com.uefs.sigsorveterias.model"})
+@Suite
+public class TestSuite {
+}
