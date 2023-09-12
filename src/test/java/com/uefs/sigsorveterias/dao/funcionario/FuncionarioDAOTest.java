1dcac1581d3216213847022a2cd3f1a3c21dcb93
Rafael Tosta <rafa.ecomp@gmail.com>
2023-09-12 11:21:46 -0300

    :construction: test: Criação de classes de testes

---
+++ src/test/java/com/uefs/sigsorveterias/dao/funcionario/FuncionarioDAOTest.java
@@ -0,0 +1,42 @@
+package com.uefs.sigsorveterias.dao.funcionario;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FuncionarioDAOTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void deleteMany() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void findMany() {
+    }
+
+    @Test
+    void findById() {
+    }
+}
---
+++ src/test/java/com/uefs/sigsorveterias/model/EstoqueTest.java
@@ -0,0 +1,36 @@
+package com.uefs.sigsorveterias.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EstoqueTest {
+
+    @Test
+    void novoProduto() {
+    }
+
+    @Test
+    void removeProduto() {
+    }
+
+    @Test
+    void removeProdutos() {
+    }
+
+    @Test
+    void updateProduto() {
+    }
+
+    @Test
+    void getProdutos() {
+    }
+
+    @Test
+    void getProduto() {
+    }
+
+    @Test
+    void getAlertaProdutos() {
+    }
+}
