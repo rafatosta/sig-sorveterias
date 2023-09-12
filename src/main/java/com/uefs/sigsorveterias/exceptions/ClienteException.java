93ae496edfafdbbf037c8a52a3f30dbcb1e839ba
Rafael Tosta <rafa.ecomp@gmail.com>
2023-09-12 11:19:26 -0300

    :sparkles: feat: mensagens padronizadas para erro para Cliente

--- src/main/java/com/uefs/sigsorveterias/exceptions/ClienteException.java
+++ src/main/java/com/uefs/sigsorveterias/exceptions/ClienteException.java
@@ -3,6 +3,10 @@ package com.uefs.sigsorveterias.exceptions;
 import com.uefs.sigsorveterias.model.Cliente;
 
 public class ClienteException extends Exception{
+
+    /** Mensagens estáticas para as exceções*/
+    public static final String DELETE = "Operação de EXCLUSÃO não realizada.";
+    public static final String UPDATE = "Operação de ATUALIZAÇÃO não realizada.";
     private Cliente cliente;
 
     public ClienteException(String message, Cliente cliente) {
